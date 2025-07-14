55f13d5469c21633d2bfcd34dc0ec7d3ad37af1e
CandhyFadhila <candhy.fadhila.arsyad@gmail.com>
2025-07-14 15:38:19 +0700

    FIXED: Delete onDelete on qurban and aqiqah product migration

--- database/migrations/2025_06_21_115020_create_qurban_products_table.php
+++ database/migrations/2025_06_21_115020_create_qurban_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('qurban_products', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('animal_id')->constrained('animals')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('animal_id')->constrained('animals')->onUpdate('cascade');
             $table->json('photo_product_id')->nullable();
             $table->string('name');
             $table->text('description');
